2f9be58d8bb4fb0789433e986ac3042af682f109
whatitisnt <83543715+whatitisnt@users.noreply.github.com>
2022-02-19 15:55:22 +0900

    Create auto-next-track.js

---
+++ script/auto-next-track.js
@@ -0,0 +1,59 @@
+<script>
+
+$('audio#track1').on('ended', function(){
+  $( '.track2' ).click();
+});
+
+$('audio#track2').on('ended', function(){
+  $( '.track3' ).click();
+});
+
+$('audio#track3').on('ended', function(){
+  $( '.track4' ).click();
+});
+
+$('audio#track4').on('ended', function(){
+  $( '.track5' ).click();
+});
+
+$('audio#track5').on('ended', function(){
+  $( '.track6' ).click();
+});
+
+$('audio#track6').on('ended', function(){
+  $( '.track7' ).click();
+});
+
+$('audio#track7').on('ended', function(){
+  $( '.track8' ).click();
+});
+
+$('audio#track8').on('ended', function(){
+  $( '.track9' ).click();
+});
+
+$('audio#track9').on('ended', function(){
+  $( '.track10' ).click();
+});
+
+$('audio#track10').on('ended', function(){
+  $( '.track11' ).click();
+});
+
+$('audio#track11').on('ended', function(){
+  $( '.track12' ).click();
+});
+
+$('audio#track12').on('ended', function(){
+  $( '.track13' ).click();
+});
+
+$('audio#track13').on('ended', function(){
+  $( '.track14' ).click();
+});
+
+$('audio#track14').on('ended', function(){
+  $( '.track1' ).click();
+});
+
+</script>
